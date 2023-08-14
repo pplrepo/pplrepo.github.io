2f9fc6cfabc3cb2d4fc01b43c9b764264eebd131
pplrepo <82588084+pplrepo@users.noreply.github.com>
2023-08-13 23:43:07 -0400

    Add files via upload

---
+++ list.js
@@ -0,0 +1,17 @@
+let listDictionary = 
+{
+    //Name Website Title Business_name Birthrates Phone Location Zip?
+    //////////////////////////////////
+    //    DO NOT FORGET COMMAS
+    //////////////////////////////////
+    "Jane Doe": {name: "Joy Thomas", business: "Postpartum Lifestyle, LLC", title: "Birth & Postpartum Doula", website: "https://www.postpartumlifestyle.com", location: "Greensboro, NC", phone: "336-355-7155", birthrate: "$600", postpartumrate: "$40/hr", zip: "27405", pic: "images/ppl.hs.JPG",},
+    "Jane DoeTwo": {name: "Jane Doe", business: "Yahoo", title: "Doula", website: "dot2.com", location: "NC, USA", phone: "123-456-7891", birthrate: "$800", postpartumrate: "$400", zip: "27410", pic: "images/electric.jpg", },
+    "Jane DoeThree": {name: "Jane Doe", business: "Meta", title: "Doula", website: "do3.com", location: "NC, USA", phone: "123-456-7891", birthrate: "$800", postpartumrate: "$400", zip: "27405", pic: "images/program.png",},
+    "Jane DoeThre": {name: "Jane Doe", business: "Amazon", title: "Doula", website: "d3.com", location: "NC, USA", phone: "123-456-7891", birthrate: "$800", postpartumrate: "$400", zip: "27405", pic: "images/program.png",},
+    "Jane DoeThr": {name: "Jane Doe", business: "Microsoft", title: "Doula", website: "dot.com", location: "NC, USA", phone: "123-456-7891", birthrate: "$800", postpartumrate: "$400", zip: "27406", pic: "images/program.png",},
+    "Jane DoeTh": {name: "Jane Doe", business: "Epic", title: "Doula", website: "ot3.com", location: "NC, USA", phone: "123-456-7891", birthrate: "$800", postpartumrate: "$400", zip: "27406", pic: "images/program.png",},
+    "Jane DoeT": {name: "Jane Doe", business: "XYZ", title: "Doula", website: "dt3.com", location: "NC, USA", phone: "123-456-7891", birthrate: "$800", postpartumrate: "$400", zip: "", pic: "images/program.png",},
+    "Jane DoeThreee": {name: "Jane Doe", business: "XYZ", title: "Doula", website: "dt3.com", location: "NC, USA", phone: "123-456-7891", birthrate: "$800", postpartumrate: "$400", zip: "", pic: "images/program.png",},
+    "Jane DoeThreee1": {name: "Jane Doe", business: "XYZ", title: "Doula", website: "dt3.com", location: "NC, USA", phone: "123-456-7891", birthrate: "$800", postpartumrate: "$400", zip: "", pic: "images/program.png",},
+
+}
